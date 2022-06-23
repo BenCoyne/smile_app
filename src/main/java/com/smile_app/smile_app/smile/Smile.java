c5529ddd3341e1f34142d117c3b72196b87ddb3e
Ben Coyne <bencoyne123@yahoo.co.uk>
2022-06-23 15:02:02 +0100

    smile class and tests running

---
+++ src/main/java/com/smile_app/smile_app/smile/Smile.java
@@ -0,0 +1,33 @@
+package com.smile_app.smile_app.smile;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Smile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Boolean isCrying;
+    private Boolean isLaughing;
+
+    public Smile(Boolean isCrying, Boolean isLaughing) {
+        this.isCrying = isCrying;
+        this.isLaughing = isLaughing;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Boolean getIsCrying() {
+        return isCrying;
+    }
+
+    public Boolean getIsLaughing() {
+        return isLaughing;
+    }
+
+}
