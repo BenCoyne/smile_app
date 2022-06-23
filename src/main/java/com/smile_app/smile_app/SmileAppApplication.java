77673a4320c49fb940494b2e012565db54103fe2
Ben Coyne <bencoyne123@yahoo.co.uk>
2022-06-23 13:39:27 +0100

    first commit

---
+++ src/main/java/com/smile_app/smile_app/SmileAppApplication.java
@@ -0,0 +1,13 @@
+package com.smile_app.smile_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmileAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmileAppApplication.class, args);
+	}
+
+}
