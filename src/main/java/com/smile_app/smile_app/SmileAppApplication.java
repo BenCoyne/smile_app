c9edb10e0b66f83ab4b4f5eac7439ece05621ae4
Ben Coyne <bencoyne123@yahoo.co.uk>
2022-06-27 10:49:04 +0100

    experiment with fianlizing classes

--- src/main/java/com/smile_app/smile_app/SmileAppApplication.java
+++ src/main/java/com/smile_app/smile_app/SmileAppApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmileAppApplication {
+public final class SmileAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmileAppApplication.class, args);
--- src/main/java/com/smile_app/smile_app/smile/SmileController.java
+++ src/main/java/com/smile_app/smile_app/smile/SmileController.java
@@ -11,7 +11,7 @@ import org.springframework.web.server.ResponseStatusException;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
-public class SmileController {
+public final class SmileController {
 
     @Autowired
     private SmileRepository smileRepository;
