0c1d8068c8d095c6662b0bd6c0dd96328dc6b074
Ben Coyne <bencoyne123@yahoo.co.uk>
2022-06-27 11:18:22 +0100

    changed linter folder name

---
+++ src/main/java/SmileAppApplication.java
@@ -0,0 +1,12 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmileAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmileAppApplication.class, args);
+	}
+
+}
